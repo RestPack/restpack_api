42ece6819d5380f0e340e49203416879f8043f6d
Gavin Joyce <gavinjoyce@gmail.com>
2013-09-21 19:05:57 +0100

    removed .json from endpoints

--- activity_api.rb
+++ activity_api.rb
@@ -1,27 +1,27 @@
 require './base'
 
 class ActivityApi < Base
-  get "/.json" do
+  get "/" do
     authorize!
     render Commands::Activity::List.run(params, application_params(request))
   end
 
-  get "/:id.json" do
+  get "/:id" do
     authorize!
     render Commands::Activity::Get.run(params, application_params(request))
   end
 
-  post "/.json" do
+  post "/" do
     authorize!
     render Commands::Activity::Create.run(params, application_params(request))
   end
 
-  put "/:id.json" do
+  put "/:id" do
     authorize!
     render Commands::Activity::Update.run(params, application_params(request))
   end
 
-  delete "/:id.json" do
+  delete "/:id" do
     authorize!
     render Commands::Activity::Destroy.run(params, application_params(request))
   end
